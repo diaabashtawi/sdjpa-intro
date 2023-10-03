71c78900d5e67f56d0cd4a17b94db3ef4851fdec
diaabashtawi <diaabashtawi@gmail.com>
2023-10-03 22:39:10 +0400

    creating the spring data jap project

---
+++ src/test/java/org/deya/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deya.sdjpaintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
