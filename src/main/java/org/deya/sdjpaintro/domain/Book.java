303a82ea8f7f10db955e8a7246a16c584a5b96ad
diaabashtawi <diaabashtawi@gmail.com>
2023-10-03 22:48:39 +0400

    adding the book entity to domain package

---
+++ src/main/java/org/deya/sdjpaintro/domain/Book.java
@@ -0,0 +1,56 @@
+package org.deya.sdjpaintro.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String isbn;
+	private String publisher;
+	
+	public Book() {
+		super();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", isbn=" + isbn + ", publisher=" + publisher + "]";
+	}
+	
+	
+	
+	
+	
+
+}
