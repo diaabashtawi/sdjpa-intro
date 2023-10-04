0c2619142e01fdd81ac1c26161c86b5baee98ea2
diaabashtawi <diaabashtawi@gmail.com>
2023-10-04 23:17:04 +0400

    adding hash and equal methods

--- src/main/java/org/deya/sdjpaintro/domain/Book.java
+++ src/main/java/org/deya/sdjpaintro/domain/Book.java
@@ -9,14 +9,20 @@ import javax.persistence.Id;
 public class Book {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String title;
 	private String isbn;
 	private String publisher;
 	
 	public Book() {
-		super();
+		
+	}
+	
+	public Book(String title, String isbn, String publisher) {
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
 	}
 
 	public String getTitle() {
