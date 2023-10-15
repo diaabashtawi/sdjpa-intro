35eb05070f7cad39b4445f9619b4c27ed69071c0
diaabashtawi <diaabashtawi@gmail.com>
2023-10-15 17:02:21 +0400

    adding the testJpaTestSliceTransaction method

--- src/test/java/org/deya/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/org/deya/sdjpaintro/SpringBootJpaTestSlice.java
@@ -3,18 +3,27 @@ package org.deya.sdjpaintro;
 
 import org.deya.sdjpaintro.domain.Book;
 import org.deya.sdjpaintro.repository.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@TestMethodOrder(
+        MethodOrderer.OrderAnnotation.class
+)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+    @Rollback(value = false)
+    @Order(1)
     @Test
     public void testJpaTestSlice() {
         long countBefore = bookRepository.count();
@@ -28,6 +37,13 @@ public class SpringBootJpaTestSlice {
         long countAfter = bookRepository.count();
         assertThat(countBefore).isLessThan(countAfter);
 
+    }
+
+    @Order(2)
+    @Test
+    public void testJpaTestSliceTransaction(){
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 
 }
